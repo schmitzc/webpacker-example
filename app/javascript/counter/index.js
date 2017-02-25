6a89101de9846cc8a9b11923d4e03665cba60eea
Cassie Koomjian <schmitzc@gmail.com>
2017-02-24 17:13:21 -0800

    Add Rails app with webpacker

---
+++ app/javascript/counter/index.js
@@ -0,0 +1,7 @@
+// Initialize the counter code when DOM is ready
+import './style.sass';
+import counter from './counter';
+
+document.addEventListener('DOMContentLoaded', () => {
+  counter.initialize();
+});
